bdf7f4db3cec0d089501a1b932fd9057d5f40e43
Jake Goritski <jakeg@proton.me>
2025-08-28 20:44:26 -0700

    update to bun

--- src/app.js
+++ src/app.js
@@ -3,5 +3,28 @@ import $ from "jquery";
 import Backbone from "backbone";
 
 $(function() {
- $("#div-1").html("Hello, bun!");
+  const Model = Backbone.Model.extend({
+    defaults: function() {
+      return {
+        "x": 1,
+        "y": 0
+      }
+    },
+    moveX: function(n) {
+      this.set({x: this.get("x") + n});
+    },
+    moveY: function(n) {
+      this.set({y: this.get("y") + n})
+    },
+    getInfo: function() {
+      console.log(`I am at (${this.get("x")}, ${this.get("y")})`);
+    }
+  });
+
+  $("h1").html("Hello, bun!");
+
+  var m = new Model();
+  m.getInfo();
+  m.moveX(10);
+  m.getInfo();
 });
